83b066e15fd7fbd1996f165211e2087170277619
csabytedev <csabytedev@gmail.com>
2013-11-05 01:04:17 +0100

    Minimal project with built-in gtest building with autotools

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "gtest/gtest.h"
 using namespace std;
 
-void main()
-{
-  cout << "Hello World!" << endl;   cout << "Welcome to C++ Programming" << endl; 
+
+TEST(FactorialTest, Zero) {
+  EXPECT_EQ(1, 1);
 }
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
